76311b906ba2bbe50dbcfcc35ae29eb79cfb3f97
Yuvraj <kingyuvi88@gmail.com>
2022-12-28 23:59:58 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,12 +1,80 @@
-
+import { useState } from "react";
+import axios from 'axios';
 import './input.css'
 import Nav from './components/Nav'
 import Form from './components/Form'
 function App() {
+  const [from, setFrom] = useState("Janak Puri");
+  const [to, setTo] = useState("Tagore Garden");
+  //DMA = Distance Matrix API
+  const DMAkey = 'JuNkCXmB4VXmxBy3rsUkrspPpnYYj';
+  const DMAurl =`https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${to}&destinations=${to}&departure_time=now&key=JuNkCXmB4VXmxBy3rsUkrspPpnYYj`; 
+
+  //WA = Weather API
+  const WAkey = 'b99b652d448e47e5a48a226c5ed84910'; 
+  const WAurl = `https://api.openweathermap.org/data/2.5/weather?q=meghalaya&appid=${WAkey}`;
+
+  let luggage = false;
+  
+ function updateFrom(e){
+     let newFrom = e.target.value;
+     setFrom(newFrom); 
+  }
+
+ function updateTo(e){
+      let newTo = e.target.value; 
+      setTo(newTo); 
+  }
+
+  function toggleLuggage(){
+      luggage = !luggage
+  }
+
+  function onSubmit(e){
+      e.preventDefault();
+      axios.get(DMAurl).then((res)=>{
+          let distance = 5;
+          console.log(res?.data?.rows[0]?.elements[0]?.distance);
+          distance = distance.split(" ");
+          distance = distance[0];
+          calculatePrice(distance);
+      })
+      axios.get(WAurl).then(response=>{
+          console.log(response.data.weather); 
+      })
+  }
+
+  function calculatePrice(distance){
+  let price = 25;
+
+      if(distance<=2 && !luggage){
+          console.log(price, luggage);
+      }
+      else if(distance<=2 && luggage){
+         price = price+7.50;
+          
+          console.log(price+"2",luggage);
+
+      }
+      else if(distance>2 && !luggage){
+         price = price + (8*(distance-2))
+          console.log(price);
+          console.log(price+" 3",luggage);
+
+      }
+      else if(distance>2 && luggage){
+          price = price + (8*(distance-2)) +7.5;
+
+          console.log(price+"4");
+      }
+  }
+
+
   return (
     <div className="App">
     <Nav/>
-    <Form/>
+    <Form 
+    from={from} to={to} toggleLuggage={toggleLuggage} updateFrom={updateFrom} updateTo={updateTo} onSubmit={onSubmit} />
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,86 +1,29 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import axios from 'axios';
-function Form() {
-    const [from, setFrom] = useState("Janak Puri");
-    const [to, setTo] = useState("Tagore Garden");
-    const key = 'JuNkCXmB4VXmxBy3rsUkrspPpnYYj';
-    const url = `https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${from}&destinations=${to}&departure_time=now&key=JuNkCXmB4VXmxBy3rsUkrspPpnYYj`; 
-    let luggage = false;
-    
-   function updateFrom(e){
-       let newFrom = e.target.value;
-       setFrom(newFrom); 
-    }
 
-   function updateTo(e){
-        let newTo = e.target.value; 
-        setTo(newTo); 
-    }
-
-    function toggleLuggage(){
-        luggage = !luggage
-    }
-
-    function onSubmit(e){
-        e.preventDefault();
-        axios.get(url).then((res)=>{
-            let d = res?.data?.rows[0]?.elements[0]?.distance.text;
-            d = d.split(" ");
-            d = d[0];
-            calculatePrice(d);
-        })
-    }
-
-    function calculatePrice(d){
-    let price = 25;
-
-        if(d<=2 && !luggage){
-            console.log(price, luggage);
-        }
-        else if(d<=2 && luggage){
-           price = price+7.50;
-            
-            console.log(price+"2",luggage);
-
-        }
-        else if(d>2 && !luggage){
-           price = price + (8*(d-2))
-            console.log(price);
-            console.log(price+" 3",luggage);
-
-        }
-        else if(d>2 && luggage){
-            price = price + (8*(d-2)) +7.5;
-
-            console.log(price+"4");
-        }
-    }
-
-
+function Form(props) {
+   
 
   return (
     <>
       <form>
         <div className="w-full items-center flex flex-col justify-center my-4">
           <input
-            value = {from} onChange={updateFrom}
+            value = {props.from} onChange={props.updateFrom}
             className="block border-2 border-black h-10 w-64 p-2"
             placeholder="Pick up location"
           />
           <br />
           <input
-            value ={to} onChange={updateTo}
+            value ={props.to} onChange={props.updateTo}
             className="border-2 border-black h-10 w-64 p-2"
             placeholder="Drop location"
           />
           <br />
           <div className="flex">
             <label className="mx-2">Luggage: </label>
-            <input onClick={toggleLuggage} type="checkbox" />
+            <input onClick={props.toggleLuggage} type="checkbox" />
           </div>
           <button
-          onClick={onSubmit}
+          onClick={props.onSubmit}
             className="bg-green-500 cursor-pointer px-4 py-2 text-white font-bold w-64 my-2"
             type="submit"
           >
