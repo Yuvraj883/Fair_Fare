79b78ea6223225f92e41b016e620aabb01a0a537
Yuvraj <kingyuvi88@gmail.com>
2022-12-31 00:39:18 +0530

    Added Geolocation feature to access user's location.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import {useRef, useState, useEffect } from "react";
-import mapboxgl from 'mapbox-gl';
+import { useRef, useState, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
 import axios from "axios";
 import "./input.css";
 import Nav from "./components/Nav";
 import Form from "./components/Form";
 import Result from "./components/Result";
-mapboxgl.accessToken = 'pk.eyJ1IjoieXV2cmFqMDAxIiwiYSI6ImNsYzlqemRhaTFxOXYzcHA2a3Nid3ZxbzMifQ.CjcPv7HXGfVNugWpahix9Q'
+import Footer from "./components/Footer";
+mapboxgl.accessToken =
+  "pk.eyJ1IjoieXV2cmFqMDAxIiwiYSI6ImNsYzlqemRhaTFxOXYzcHA2a3Nid3ZxbzMifQ.CjcPv7HXGfVNugWpahix9Q";
 function App() {
-  useEffect(() => {
-    if (map.current) return; // initialize map only once
-    map.current = new mapboxgl.Map({
-    container: mapContainer.current,
-    style: 'mapbox://styles/mapbox/streets-v12',
-    center: [lng, lat],
-    zoom: zoom
-    });
-    });
-const mapContainer = useRef(null);
-const map = useRef(null);
-const [lng, setLng] = useState(-70.9);
-const [lat, setLat] = useState(42.35);
-const [zoom, setZoom] = useState(9);
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const [lng, setLng] = useState(77.0801664);
+  const [lat, setLat] = useState(28.639232);
+  const [zoom, setZoom] = useState(9);
   const [from, setFrom] = useState("Janak Puri");
   const [to, setTo] = useState("Tagore Garden");
   const [distance, setDistance] = useState(1);
@@ -28,7 +21,7 @@ const [zoom, setZoom] = useState(9);
 
   //DMA = Distance Matrix API
   const DMAkey = "JuNkCXmB4VXmxBy3rsUkrspPpnYYj";
-  const DMAurl = `https://api.distancematrix.ai/maps/api/distancematrix/json?origins=${to}&destinations=${to}&departure_time=now&key=${DMAkey}`;
+  const DMAurl = `https://api.distancematrix.ai/maps/api/distancematrix/jsonorigins=${to}&destinations=${from}&departure_time=now&key=${DMAkey}`;
 
   //WA = Weather API
   const WAkey = "b99b652d448e47e5a48a226c5ed84910";
@@ -36,6 +29,30 @@ const [zoom, setZoom] = useState(9);
 
   let luggage = false;
 
+  useEffect(() => {
+    const successCallback = (position) => {
+      setLat(position.coords.latitude); 
+      setLng (position.coords.longitude);
+    };
+
+    const errorCallback = (error) => {
+      console.log(error);
+    };
+
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  });
+
+  useEffect(() => {
+    if (map.current) return; // initialize map only once
+    map.current = new mapboxgl.Map({
+      container: mapContainer.current,
+      style: "mapbox://styles/mapbox/streets-v12",
+      center: [lng, lat],
+      zoom: zoom,
+    });
+  });
+
+  //Getting the user's current location
   function updateFrom(e) {
     let newFrom = e.target.value;
     setFrom(newFrom);
@@ -79,11 +96,11 @@ const [zoom, setZoom] = useState(9);
   }
   let price = 25;
   function calculatePrice(distance) {
-     price = 25;
-     let date = new Date();
-     let time= date.toLocaleTimeString();
+    price = 25;
+    let date = new Date();
+    let time = date.toLocaleTimeString();
     //  time = time/(1000*60*60*24*365);
-     console.log("Date: "+time);
+    console.log("Date: " + time);
 
     if (distance <= 2 && !luggage) {
       price = price;
@@ -93,18 +110,14 @@ const [zoom, setZoom] = useState(9);
       console.log(price + "2", luggage);
     } else if (distance > 2 && !luggage) {
       price = price + 8 * (distance - 2);
-     
     } else if (distance > 2 && luggage) {
       price = price + 8 * (distance - 2) + 7.5;
-
     }
     let start = new Date();
 
-    
-    if(time>="02:00:00 AM" ){
+    if (time >= "02:00:00 AM") {
       console.log("greater");
-    }
-    else{
+    } else {
       console.log("lesser");
     }
   }
@@ -121,13 +134,10 @@ const [zoom, setZoom] = useState(9);
         onSubmit={onSubmit}
       />
       <div>
-<div ref={mapContainer} className="map-container h-[400px]" />
-</div>
-      <Result
-      distance={distance}
-      duration={duration}
-      price={price}
-      />
+        <div ref={mapContainer} className="map-container h-[400px] w-[80%] m-auto m-4" />
+      </div>
+      <Result distance={distance} duration={duration} price={price} />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer(){
+   return( <>
+    <footer className="bg-black text-white text-center">
+        <div>Yuvraj Singh&copy;2022</div>
+
+    </footer>
+    </>
+   )
+}
+export default Footer;
